24ff8101941159c15358ea238042aac38df5b065
Ori Roitzaid <oriroitzaid@gmail.com>
2023-10-31 14:31:07 +0200

    Finished the design of the main activity

--- app/src/main/java/com/example/ex1101/MainActivity.java
+++ app/src/main/java/com/example/ex1101/MainActivity.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author Ori Roitzaid <or1901 @ bs.amalnet.k12.il>
+ * @version	1
+ * @since 31/10/2023
+ * The main activity:
+ * Displays a counter and a text field, allows the user to change the both of them with buttons.
+ * Saves the data when the user exits the app, and loads it again when the app runs.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
